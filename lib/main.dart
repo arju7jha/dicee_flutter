d9592f1a586aafbe3f99df5eb70820cf21f47cae
Arju Kumar <88245601+arju7jha@users.noreply.github.com>
2023-08-18 21:45:54 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,32 @@
+
 import 'dart:math';
 import 'package:flutter/material.dart';
 
-void main() {
-  return runApp(
-    MaterialApp(
+void main() => runApp(Dicee());
+
+class Dicee extends StatelessWidget {
+  const Dicee({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.teal[300],
-        appBar: AppBar(
-          title: Text('DICE-ROLLER'),
-          backgroundColor: Colors.teal,
-        ),
-        body: DicePage(),
-      ),
-    ),
-  );
+      theme: ThemeData.light(),
+      home: DicePage(),
+    );
+  }
 }
 
+
 class DicePage extends StatefulWidget {
-  const DicePage({Key? key}) : super(key: key);
+  const DicePage({super.key});
 
   @override
   State<DicePage> createState() => _DicePageState();
 }
 
 class _DicePageState extends State<DicePage> {
+
   int LeftDiceNumber = 1;
   int RightDiceNumber = 1;
 
@@ -34,31 +36,58 @@ class _DicePageState extends State<DicePage> {
       RightDiceNumber = Random().nextInt(6)+1;
     });
   }
+  // void ChangeDice2() {
+  //   setState(() {
+  //     //LeftDiceNumber = Random().nextInt(6)+1; //here by using '+1' it will increase the value by adding 1 from 0 to 6
+  //     RightDiceNumber = Random().nextInt(6)+1;
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: <Widget>[
-
-          Expanded(
-            child: TextButton(
-              onPressed: () {
-                ChangeDice();
-              },
-              child: Image.asset('images/dice$LeftDiceNumber.png'),
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DICE - ROLLER',
+          style: TextStyle(
+            color: Colors.black,
           ),
-
-          Expanded(
-            child: TextButton(
-              onPressed: () {
-                ChangeDice();
-              },
-              child: Image.asset('images/dice$RightDiceNumber.png'),
-            ),
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.black,
+          image: DecorationImage(
+            image: AssetImage('images/dicee bg3.jpg'),
+            fit: BoxFit.cover,
           ),
-        ],
+        ),
+        child: Center(
+          child: Row(
+            children: <Widget>[
+
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    ChangeDice();
+                  },
+                  child: Image.asset('images/dice$LeftDiceNumber.png'),
+                ),
+              ),
+
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    ChangeDice();
+                  },
+                  child: Image.asset('images/dice$RightDiceNumber.png'),
+                ),
+              ),
+
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
